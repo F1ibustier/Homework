904344906fe4e273c3ab41347bb8f7f8c416ee85
F1ibustier <victor07378@gmail.com>
2024-06-27 13:29:04 +0300

    Add files via upload
    Домашняя работа по уроку "Базовые структуры данных" из вводного курса

---
+++ main.py
@@ -0,0 +1,12 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd programm
+a = 1234
+b = 5678
+print(a % 1000 // 10 + b % 5000 // 10)
+#4th program
+c = '13.42'
+d = '42.13'
+print(int(c[0:2]) == int (d[3:]) or int(c[3:]) == int(d[0:2]))
