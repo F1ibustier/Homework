ba57b68226809ba7a53a882175827dd77ccdcaec
F1ibustier <victor07378@gmail.com>
2024-06-28 09:22:43 +0300

    Add files via upload
    Домашняя работа по уроку "Переменные" от 28.06.2024 г.

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_DZ = 12
+count_hours = 1.5
+course = 'Python'
+time_to_1_DZ = count_hours / count_DZ
+print('Курс: '+course+', ', 'всего задач: ', count_DZ, ', ', 'затрачено часов: ', count_hours, ', ', 'среднее время выполнения: ', time_to_1_DZ, 'часа')
