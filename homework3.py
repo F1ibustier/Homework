a9a7ff426426743f2b03f3e684e699041176a3f6
F1ibustier <victor07378@gmail.com>
2024-06-29 09:33:41 +0300

    Add files via upload
    Домашняя работа по уроку "Динамическая типизация" от 29.06.2024 г.

---
+++ homework3.py
@@ -0,0 +1,9 @@
+#динамическая типизация
+name = 'Виктор'
+print('Имя: ', name)
+age = 40
+print('Возраст: ', age)
+age = age + 1
+print('Возраст после окончания обучения: ', age)
+is_student = True
+print('Я студент? - ', is_student)
