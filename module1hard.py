d7bd0fcb70111f9462f97e3ba97246bc5d007c4e
F1ibustier <victor07378@gmail.com>
2024-07-02 15:29:05 +0300

    Add files via upload
    Домашняя работа по модулю "Базовые структуры данных" от 02.06.2024 г.

--- module1hard.py
+++ module1hard.py
@@ -1,11 +1,14 @@
+#Базовые структуры данных
+#coded by f1ibustier
+
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = list(students)
-students.sorted()
-yspevaemost = {students [0]:sr_ball_1, students [1]:sr_ball_2, students [2]:sr_ball_3, students [3]:sr_ball_4, students [4]:sr_ball_5}
-sr_ball_1 = sum(grades[0]/len(grades[0])
-sr_ball_2 = sum(grades[1])
-sr_ball_3 = sum(grades[2])
-sr_ball_4 = sum(grades[3])
-sr_ball_5 = sum(grades[4])
+students.sort()
+sr_ball_0 = sum(grades[0])/len(grades[0])
+sr_ball_1 = sum(grades[1])/len(grades[1])
+sr_ball_2 = sum(grades[2])/len(grades[2])
+sr_ball_3 = sum(grades[3])/len(grades[3])
+sr_ball_4 = sum(grades[4])/len(grades[4])
+yspevaemost = {students[0]: sr_ball_0, students[1]: sr_ball_1, students[2]: sr_ball_2, students[3]: sr_ball_3, students[4]: sr_ball_4}
 print(yspevaemost)
