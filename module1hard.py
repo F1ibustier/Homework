63b66f8df86d0a8321271dac0699e2bd171df137
F1ibustier <victor07378@gmail.com>
2024-07-02 10:44:23 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -3,7 +3,7 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = list(students)
 students.sorted()
 yspevaemost = {students [0]:sr_ball_1, students [1]:sr_ball_2, students [2]:sr_ball_3, students [3]:sr_ball_4, students [4]:sr_ball_5}
-sr_ball_1 = sum(grades[0])
+sr_ball_1 = sum(grades[0]/len(grades[0])
 sr_ball_2 = sum(grades[1])
 sr_ball_3 = sum(grades[2])
 sr_ball_4 = sum(grades[3])
