0866233edecde6f67e4c001c8247cada121fdb9a
F1ibustier <victor07378@gmail.com>
2024-07-02 10:27:30 +0300

    Create module1hard.py

---
+++ module1hard.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(students)
+students.sorted()
+yspevaemost = {students [0]:sr_ball_1, students [1]:sr_ball_2}
+sr_ball_1 = sum(grades[0])
+sr_ball_2 = sum(grades[1])
+sr_ball_3 = sum(grades[2])
+sr_ball_4 = sum(grades[3])
+sr_ball_5 = sum(grades[4])
