78a937bf316438ed60347c8d809e1216daff963b
F1ibustier <victor07378@gmail.com>
2024-07-04 14:30:20 +0300

    Add files via upload
    Домашняя работа по уроку "Условная конструкция. Операторы if, elif, else." от 03.06.2024 г.

---
+++ module_2_2.py
@@ -0,0 +1,21 @@
+#Цели и задачи. Поток выполнения программы.
+#coded by f1ibustier
+
+first = 123
+second = 456
+third = 789
+if first==second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+else:
+    print(0)
+first = 42
+second = 69
+third = 42
+if first==second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+else:
+    print(0)
