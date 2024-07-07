9bc1696122e25047d983593726d6fd2f29b49314
F1ibustier <victor07378@gmail.com>
2024-07-07 21:50:51 +0300

    Add files via upload
    Домашняя работа по уроку "Функции в Python.Функция с параметром" от 07.07.2024 г.

---
+++ module_2_5.py
@@ -0,0 +1,13 @@
+# Функции в Python.Функция с параметром.Документирование функции
+# coded by f1ibustier
+
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    print(matrix)
+get_matrix(2, 2, 10)
+get_matrix(3, 5, 42)
+get_matrix(4, 2, 13)
