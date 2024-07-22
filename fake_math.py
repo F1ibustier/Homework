8ecf1c866b3a2c8e9da0a6fc75e110e686c302f7
F1ibustier <victor07378@gmail.com>
2024-07-23 00:45:23 +0300

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -1,6 +1,11 @@
+# Модули и пакеты
+# coded by f1ibustier
+
+
 def divide(first, second):
     if second == 0:
         return "Ошибка"
     else:
         result = first / second
     return result
+
