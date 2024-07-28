242ab5293d0f7577f308a4df0fc2192f35180757
F1ibustier <victor07378@gmail.com>
2024-07-28 15:02:51 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -5,4 +5,5 @@ def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
     inner_function()
+#inner_function() в этом месте вызов внутренней функции выдает ошибку
 test_function()
