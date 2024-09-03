83711f3f0290c398745a475a75a2c6ed634fe95c
F1ibustier <victor07378@gmail.com>
2024-09-03 21:30:25 +0300

    Add files via upload
    Домашняя работа по уроку "Позиционирование в файле"

---
+++ module_7_2.py
@@ -0,0 +1,23 @@
+# Позиционирование в файле
+# coded by f1ibustier
+
+def custom_write(file_name, strings):
+    strings_position = {}
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i, string in enumerate(strings, start=1):
+            begin_byte = file.tell()
+            file.write(string + '\n')
+            strings_position[(i, begin_byte)] = string
+    return strings_position
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
