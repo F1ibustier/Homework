cae67acf0fd6e06782d6f8bfb8fe259fbc4abe81
F1ibustier <victor07378@gmail.com>
2024-09-30 21:30:03 +0300

    Add files via upload
    Домашняя работа по уроку "Многопроцессное программирование"

---
+++ module_10_5.py
@@ -0,0 +1,32 @@
+# Многопроцессное программирование
+# coded by f1ibustier
+from datetime import datetime
+import multiprocessing
+
+
+def read_info(name):
+    all_data = []
+    with open(name) as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            else:
+                all_data.append(line)
+
+
+file_names = [f'./file {number}.txt' for number in range(1, 5)]
+# Линейный вызов - 0:00:13.508635 c
+# start = datetime.now()
+# for file_name in file_names:
+#     read_info(file_name)
+# end = datetime.now()
+# print('Длительность выполнения программы при линейном вызове функции', end - start)
+
+# Многопроцессный вызов -
+if __name__ == '__main__':
+    start = datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, file_names)
+        end = datetime.now()
+        print('Длительность выполнения программы при многопроцессном вызове функции', end - start)
