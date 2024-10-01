5ec641d9e561b2b38faa5b57e4d4741e92057854
F1ibustier <victor07378@gmail.com>
2024-10-01 22:04:17 +0300

    module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -9,24 +9,23 @@ def read_info(name):
     with open(name) as file:
         while True:
             line = file.readline()
+            all_data.append(line)
             if not line:
                 break
-            else:
-                all_data.append(line)
 
 
 file_names = [f'./file {number}.txt' for number in range(1, 5)]
-# Линейный вызов - 0:00:13.508635 c
-# start = datetime.now()
-# for file_name in file_names:
-#     read_info(file_name)
-# end = datetime.now()
-# print('Длительность выполнения программы при линейном вызове функции', end - start)
+# Линейный вызов
+start = datetime.now()
+for file_name in file_names:
+    read_info(file_name)
+end = datetime.now()
+print('Длительность выполнения программы при линейном вызове функции', end - start)
 
-# Многопроцессный вызов -
+# Многопроцессный вызов
 if __name__ == '__main__':
     start = datetime.now()
     with multiprocessing.Pool(processes=4) as pool:
         pool.map(read_info, file_names)
-        end = datetime.now()
-        print('Длительность выполнения программы при многопроцессном вызове функции', end - start)
+    end = datetime.now()
+    print('Длительность выполнения программы при многопроцессном вызове функции', end - start)
