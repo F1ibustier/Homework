71ec3626c3f58e59338f533dae56093e1c4d6420
F1ibustier <victor07378@gmail.com>
2024-12-07 21:53:07 +0300

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -1,18 +1,86 @@
 # Классы и объекты
 # coded by f1ibustier
+import time
+
 
 class User:
     def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = hash(password)
+        self.age = age
+
+    def __str__(self):
+        return self.nickname
+
+    def __repr__(self):
+        return f'User(nickname={self.nickname}, age={self.age})'
 
 
 class Video:
-    def __init__(self, title, duration, time_now=0, adult_mode=False):
+    def __init__(self, title, duration, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f'Video(title={self.title}, duration={self.duration}, adult_mode={self.adult_mode})'
 
 
 class UrTube:
-    def __init__(self, users, videos, current_user):
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
 
+    def log_in(self, nickname, password):
+        for user in self.users:
+            if nickname == user.nickname and hash(password) == hash(user.password):
+                self.current_user = user
+                return
 
+    def register(self, nickname, password, age):
+        password = hash(password)
+        for user in self.users:
+            if nickname == user.nickname:
+                print(f'Пользователь {nickname} уже существует')
+                return
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.current_user = new_user
+        print(f'Пользователь {nickname} зарегистрирован и вошел в систему')
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, *videos):
+        for video in videos:
+            if not any(v.title == video.title for v in self.videos):
+                self.videos.append(video)
+
+    def get_videos(self, search_word):
+        return [video.title for video in self.videos if search_word.lower() in video.title.lower()]
+
+    def watch_video(self, video_name):
+        if self.current_user is None:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+            return
+        for video in self.videos:
+            if video.title == video_name:
+                if video.adult_mode == True and self.current_user.age < 18:
+                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                    return
+                while video.time_now < video.duration:
+                    print(f' {video.time_now + 1}')
+                    video.time_now += 1
+                    time.sleep(0.5)
+                print('Конец видео')
+                video.time_now = 0
+                return
+        print('Видео не найдено')
 
 
 ur = UrTube()
