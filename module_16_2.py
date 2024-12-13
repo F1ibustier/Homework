709a55887d98398b0a9182c5c111e8dd910f3ced
F1ibustier <victor07378@gmail.com>
2024-12-13 12:14:14 +0300

    module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -4,18 +4,18 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get("/")
-async def main_page():
+async def main_page() -> str:
     return "Главная страница"
 
 @app.get("/user/admin")
-async def admin_login():
+async def admin_login() -> str:
     return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
 async def users_login(user_id: Annotated[int, Path(ge=1,
                                                    le=100,
                                                    description='Enter User ID',
-                                                   example=5)]):
+                                                   example=5)]) -> str:
     return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user/{username}/{age}")
@@ -26,5 +26,5 @@ async def users_info(username: Annotated[str, Path(min_length=5,
                      age: Annotated[int, Path(ge=18,
                                               le=120,
                                               description='Enter age',
-                                              example=24)]):
+                                              example=24)]) -> str:
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
